2c60021e4cd9786f9e71f318c2b4b80357690178
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-12-29 10:47:52 +0000

    Added postCSS pipe to gulp

--- github-issues/src/Gulpfile.js
+++ github-issues/src/Gulpfile.js
@@ -1,5 +1,8 @@
+var postcss = require('gulp-postcss');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 gulp.task('styles', function() {
 	gulp
@@ -11,4 +14,12 @@ gulp.task('styles', function() {
 //Watch task
 gulp.task('default', function() {
 	gulp.watch('sass/**/*.scss', ['styles']);
+});
+
+gulp.task('css', function() {
+	var plugins = [autoprefixer({ browsers: ['last 5 version'] }), cssnano()];
+	return gulp
+		.src('css/*.css')
+		.pipe(postcss(plugins))
+		.pipe(gulp.dest('./'));
 });
