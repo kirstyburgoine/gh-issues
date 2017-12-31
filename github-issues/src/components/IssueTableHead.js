01f27ce1b2c0cf824612028b723c5fc4566d6cc3
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-12-31 13:04:04 +0000

    created dropdown, added show / hide, styled

--- github-issues/src/components/IssueTableHead.js
+++ github-issues/src/components/IssueTableHead.js
@@ -1,38 +1,97 @@
 import React, { Component } from 'react';
-import openIcon from './octicon-open.svg';
-import closedIcon from './octicon-closed.svg';
-import commentIcon from './octicon-comment.svg';
+import Author from './Author';
 
 /**
  * Form that filters the results
  */
 class IssueTableHead extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			display: false
+		};
+		this.showDropDown = this.showDropDown.bind(this); // binds this in my function to the class so that it can be used in callback
+	}
+
+	showDropDown(event) {
+		event.preventDefault();
+		// console.log('this worked');
+
+		this.setState(prevState => ({
+			display: !prevState.display
+		}));
+	}
+
 	render() {
-		// details comes from issue component markup set up in IssuesTable.js
+		// details comes from IssueTableHead component markup set up in IssuesTable.js
 		const { details } = this.props;
+		console.log(details.number);
+
+		const users = details.user;
+		const numbers = [1, 2, 3, 4, 5];
+
+		// set the state of display so we can control show / hide
+		const display = this.state.display;
 
 		return (
-			<div class="table-head">
+			<div className="table-head">
 				<div className="table-results-count" />
 				<div className="table-list-filters">
-					<button className="btn-link select-menu-button">
-						Author
-					</button>
-					<button className="btn-link select-menu-button">
-						Labels
-					</button>
-					<button className="btn-link select-menu-button" disabled>
-						Projects
-					</button>
-					<button className="btn-link select-menu-button" disabled>
-						Milestones
-					</button>
-					<button className="btn-link select-menu-button" disabled>
-						Assignee
-					</button>
-					<button className="btn-link select-menu-button" disabled>
-						Sort
-					</button>
+					<form className="list-filters">
+						<fieldset>
+							<button
+								className="btn-link select-menu-button"
+								onClick={this.showDropDown}
+							>
+								Author
+							</button>
+
+							<ul
+								className={
+									display ? 'displayblock' : 'displaynone;'
+								}
+							>
+								<Author numbers={users} />
+							</ul>
+						</fieldset>
+						<fieldset>
+							<button className="btn-link select-menu-button">
+								Labels
+							</button>
+						</fieldset>
+						<fieldset>
+							<button
+								className="btn-link select-menu-button"
+								disabled
+							>
+								Projects
+							</button>
+						</fieldset>
+						<fieldset>
+							<button
+								className="btn-link select-menu-button"
+								disabled
+							>
+								Milestones
+							</button>
+						</fieldset>
+						<fieldset>
+							<button
+								className="btn-link select-menu-button"
+								disabled
+							>
+								Assignee
+							</button>
+						</fieldset>
+						<fieldset>
+							<button
+								className="btn-link select-menu-button"
+								disabled
+							>
+								Sort
+							</button>
+						</fieldset>
+					</form>
 				</div>
 			</div>
 		);
