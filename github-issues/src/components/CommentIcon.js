dd98e1e122be75468540cc5c96a1aa787516cc57
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2018-01-01 14:37:45 +0000

    removed warnings form SVG

--- github-issues/src/components/CommentIcon.js
+++ github-issues/src/components/CommentIcon.js
@@ -3,15 +3,12 @@ import React from 'react';
 export default () => (
 	<svg
 		aria-hidden="true"
-		class="octicon octicon-comment v-align-middle"
+		className="octicon octicon-comment v-align-middle"
 		height="16"
 		version="1.1"
 		viewBox="0 0 16 16"
 		width="16"
 	>
-		<path
-			fill-rule="evenodd"
-			d="M14 1H2c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1h2v3.5L7.5 11H14c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zm0 9H7l-2 2v-2H2V2h12v8z"
-		/>
+		<path d="M14 1H2c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1h2v3.5L7.5 11H14c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zm0 9H7l-2 2v-2H2V2h12v8z" />
 	</svg>
 );
