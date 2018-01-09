0282910227f01fa9b76bb88fb2eb22a002441b96
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2018-01-09 19:09:23 +0000

    Changed base font size / added header title

--- github-issues/src/components/Header.js
+++ github-issues/src/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react';
 // Stateless Functional Component
 // Only going to output static HTML so no eed for more
 const Header = () => {
-	return <header role="banner" />;
+	return (
+		<header role="banner">
+			<p>Today's Issues</p>
+		</header>
+	);
 };
 
 export default Header;
